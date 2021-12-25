15c1dd5cf9e33405df2653a402d0d8a40246b697
Ayan <ayanreza0@gmail.com>
2021-12-25 01:37:43 -0500

    set up NFTMarket smart contract

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,44 @@
+// contracts/Market.sol
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 listingPrice = .025 ether;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemId;
+        uint256 tokenId;
+        uint256 price;
+        address nftContract;
+        address payable seller;
+        address payable owner;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        uint256 price,
+        address indexed nftContract,
+        address payable seller,
+        address payable owner,
+        bool sold
+    );
+}
