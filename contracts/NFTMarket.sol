5b1624ca165b1e8230ba725985947175d904a0a1
Ayan <ayanreza0@gmail.com>
2021-12-27 00:37:56 -0500

    returns unsold market items

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -85,4 +85,21 @@ contract NFTMarket is ReentrancyGuard {
         payable(owner).transfer(listingPrice);
 
     }
+
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].owner == address(0)) {
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
+    }
 }
